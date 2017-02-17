801a9e68cc2aab9a9c377744ddf0ff510bab1303
GoldenPuma <love.dittlau@gmail.com>
2017-02-17 14:24:46 +0100

    Automated (All) Save

--- SAVE ALL.cmd
+++ SAVE ALL.cmd
@@ -1,13 +1,15 @@
 @echo off
 cls
 
-
+color 0a
 echo ------------------
 echo  Save Initalized
 echo ------------------
+color B
 git add *
 git commit -a -m "Automated (All) Save"
 git push -u origin master
+color 0a
 echo ------------------
 echo  Save Completed
 echo ------------------
