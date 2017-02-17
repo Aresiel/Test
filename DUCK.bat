e45ff99a121723296302896afb6d1dd7f30031af
GoldenPuma <love.dittlau@gmail.com>
2017-02-17 10:20:38 +0100

    Automated (DUCK.bat) Save

--- DUCK.bat
+++ DUCK.bat
@@ -36,9 +36,152 @@ pause
 goto :ducklogin
 
 :ducksignin
+cls
 IF NOT EXIST "\DATA\duckname.txt" goto SIGNUP
 IF NOT EXIST "\DATA\duckpass.txt" goto SIGNUP
 goto LOGIN
 
 :SIGNUP
-tile SignUp
+cls
+echo ----------------------------
+echo           SignUp
+echo ----------------------------
+echo.
+set/p "duckname=Enter Username: "
+set/p "duckpass=Enter Password: "
+echo.
+echo.
+IF %duckname% == "" goto SIGNUPERROR
+IF %duckpass% == "" goto SIGNUPERROR
+goto CREATEACC
+
+:SIGNUPERROR
+cls
+echo ----------------------------
+echo           Error
+echo ----------------------------
+echo Invalid Username Or Password
+pause
+goto SIGNUP
+
+:CREATEACC
+cls
+echo %duckname% >> \DATA\duckname.txt
+echo %duckpass% >> \DATA\duckpass.txt
+attrib "\DATA\duckname" +s +h
+attrib "\DATA\duckpass" +s +h
+goto LOGIN
+
+:LOGIN
+cls
+set/p duckname= < \DATA\duckname.txt
+set/p duckpass= < \DATA\duckpass.txt
+cls
+echo ----------------------------------
+echo              Login
+echo ----------------------------------
+echo.
+echo.
+set/p "ducknameI=_        Username: "
+set/p "duckpassI=_        Password: "
+echo.
+if %ducknameI% == "" goto LOGINERROR
+if %duckpassI% == "" goto LOGINERROR
+if not %ducknameI% == %duckname% goto LOGINERROR
+if not %duckpassI% == %duckpass% goto LOGINERROR
+goto LOADING
+
+:LOGINERROR
+cls
+echo -------------------------------
+echo             Error
+echo -------------------------------
+echo Invalid Username Or Password
+pause
+goto LOGIN
+
+
+:LOADING
+set load=%load%!!!!!!
+cls
+echo ----------------------------------
+echo              Login
+echo ----------------------------------
+echo.
+echo.
+echo         Username: %ducknameI%
+echo         Password: %duckpassI%
+echo.
+echo.
+echo    ===================================
+echo    %load%
+echo    ===================================
+echo.
+echo.
+ping localhost -n 2 >nul
+set/a loadnum=%loadnum% +1
+if %loadnum% == 5 goto DONE
+goto LOADING
+
+:DONE
+cls
+echo -----------------------------------
+echo                Done
+echo -----------------------------------
+echo.
+echo        #
+echo       #
+echo  #   #      Login Successfully
+echo   # #
+echo    #
+echo.
+echo.
+echo -----------------------------------
+pause
+goto MainMenu
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
