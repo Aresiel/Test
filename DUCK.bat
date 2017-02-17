d24b9ea6248ef1f46f440f3964cdfd9eefc9913f
GoldenPuma <love.dittlau@gmail.com>
2017-02-17 09:32:55 +0100

    Automated (DUCK.bat) Save

---
+++ DUCK.bat
@@ -0,0 +1,30 @@
+@echo off
+cls
+
+
+:ducklogin
+cls
+echo ----------------
+echo    Welcome to
+echo     DUCK.bat
+echo ----------------
+echo  (1) Log In
+echo  (2) Shut Off
+echo  (3 - Dev Only) save
+set/p "ducklogin=Input: "
+
+
+if %ducklogin% == 1 goto ducksignin 
+if %ducklogin% == 2 exit
+if %ducklogin% == 3 goto dev_save
+pause
+goto ducklogin
+
+
+:dev_save
+cls
+git add DUCK.bat
+git commit -m "Automated (DUCK.bat) Save"
+git push -u origin master
+pause
+goto :ducklogin
